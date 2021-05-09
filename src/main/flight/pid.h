233128c70f4264254f9c65c3f15aae4c0a7ce3bc
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-05-10 00:25:01 +0100

    RTFL: Use I-term rotation around yaw only
    Rotate RP plane around Yaw, only.
    Rotating the tail error to cyclic does not make sense.
    
    Add USE_ITERM_ROTATION ifdef.

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -199,7 +199,6 @@ extern float axisError[XYZ_AXIS_COUNT];
 void applyItermRelax(const int axis, const float iterm,
     const float gyroRate, float *itermErrorRate, float *currentPidSetpoint);
 void applyAbsoluteControl(const int axis, const float gyroRate, float *currentPidSetpoint, float *itermErrorRate);
-void rotateItermAndAxisError();
 float pidLevel(int axis, const pidProfile_t *pidProfile,
     const rollAndPitchTrims_t *angleTrim, float currentPidSetpoint);
 float calcHorizonLevelStrength(void);
