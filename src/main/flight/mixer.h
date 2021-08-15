283124a80bc2f23be74cf97e76bbb6b04855a11f
Petri Mattila <petri.t.j.mattila@gmail.com>
2021-08-15 12:21:47 +0100

    RTFL: Refactor mixer input handling

--- src/main/flight/mixer.h
+++ src/main/flight/mixer.h
@@ -119,6 +119,7 @@ typedef struct
     uint8_t   mode;
 
     uint8_t   main_rotor_dir;   // Main rotor direction: CW/CCW
+
     uint8_t   tail_rotor_mode;  // Tail motor vs. variable pitch tail
     int16_t   tail_motor_idle;  // Idle throttle for tail motor
 
