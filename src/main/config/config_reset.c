b14205fcd81832b8fc097871918591109bb096e6
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-06 11:40:21 +0100

    RTFL: Add support for erasing config without full erase
    When a Makefile flag FLASH_CONFIG_ERASE=yes is defined,
    the generated .hex/.bin file will overwrite the config
    area in the flash, effectively erasing the config.
    
    This flag is supposed to be used with the snapshot images.
    
    During development the config flash structure is often
    changed, which invalidates any existing content in the flash.
    This will show up as corrupted config in the CLI.
    
    The best way to avoid surprise corruption is to erase the
    config with the development images, and force the user
    to reconfigure or reload the config from a text dump.
    
    Release images should _NEVER_ use this flag!

---
+++ src/main/config/config_reset.c
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "platform.h"
+
+#include "build/build_config.h"
+
+/*
+ * Having anything in .flash_config section makes the flashing tool
+ * to erase the whole sector, effectively erasing the config.
+ */
+
+#ifdef USE_CONFIG_ERASE
+
+#define CONFIG_FLASH __attribute__ ((section(".flash_config"), aligned(4), used))
+
+static CONFIG_FLASH uint32_t erase_magic = 0xffffffff;
+
+#endif
