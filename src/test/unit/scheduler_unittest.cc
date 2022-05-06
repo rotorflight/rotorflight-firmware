SteveEvans-eaton/betaflight

Trigger gyro SPI DMA reads in EXTI handler if supported and lock gyroTask loop to gyro to eliminate missed updates and jitter

src/test/unit/scheduler_unittest.cc
@@ -51,10 +51,14 @@ extern "C" {
     bool taskPidRan = false;
     bool taskFilterReady = false;
     bool taskPidReady = false;
+    uint8_t activePidLoopDenom = 1;
 
     // set up micros() to simulate time
     uint32_t simulatedTime = 0;
     uint32_t micros(void) { return simulatedTime; }
+    int32_t clockCyclesToMicros(int32_t x) { return x/10;}
+    int32_t clockMicrosToCycles(int32_t x) { return x*10;}
+    int32_t getCycleCounter(void) {return simulatedTime * 10;}
 
     // set up tasks to take a simulated representative time to execute
     bool gyroFilterReady(void) { return taskFilterReady; }
@@ -353,6 +357,7 @@ TEST(SchedulerUnittest, TestSingleTask)
     }
     setTaskEnabled(TASK_ACCEL, true);
     tasks[TASK_ACCEL].lastExecutedAtUs = 1000;
+    tasks[TASK_ACCEL].lastStatsAtUs = 1000;
     simulatedTime = 2050;
     // run the scheduler and check the task has executed
     scheduler();
@@ -462,6 +467,7 @@ TEST(SchedulerUnittest, TestGyroTask)
 
     // run the scheduler
     scheduler();
+
     // the gyro task indicator should be true and the TASK_FILTER and TASK_PID indicators should be false
     EXPECT_TRUE(taskGyroRan);
     EXPECT_FALSE(taskFilterRan);

rotorflight/rotorflight-firmware

src/test/unit/scheduler_unittest.cc
@@ -51,10 +51,18 @@ extern "C" {
     bool taskPidRan = false;
     bool taskFilterReady = false;
     bool taskPidReady = false;
+    uint8_t activePidLoopDenom = 1;
+
+    int16_t debug[1];
+    uint8_t debugMode = 0;
 
     // set up micros() to simulate time
     uint32_t simulatedTime = 0;
     uint32_t micros(void) { return simulatedTime; }
+    uint32_t clockCyclesToMicros(uint32_t x) { return x/10;}
+    int32_t clockCyclesTo10thMicros(int32_t x) { return x;}
+    uint32_t clockMicrosToCycles(uint32_t x) { return x*10;}
+    uint32_t getCycleCounter(void) {return simulatedTime * 10;}
 
     // set up tasks to take a simulated representative time to execute
     bool gyroFilterReady(void) { return taskFilterReady; }
@@ -463,6 +471,7 @@ TEST(SchedulerUnittest, TestGyroTask)
 
     // run the scheduler
     scheduler();
+
     // the gyro task indicator should be true and the TASK_FILTER and TASK_PID indicators should be false
     EXPECT_TRUE(taskGyroRan);
     EXPECT_FALSE(taskFilterRan);
@@ -507,102 +516,3 @@ TEST(SchedulerUnittest, TestGyroTask)
     EXPECT_EQ(static_cast<task_t*>(0), unittest_scheduler_selectedTask);
 }
 
-// Test the scheduling logic that prevents other tasks from running if they
-// might interfere with the timing of the next gyro task.
-TEST(SchedulerUnittest, TestGyroLookahead)
-{
-    static const uint32_t startTime = 4000;
-
-    // enable task statistics
-    schedulerSetCalulateTaskStatistics(true);
-
-    // disable scheduler optimize rate
-    schedulerOptimizeRate(false);
-
-    // enable the gyro
-    schedulerEnableGyro();
-
-    // disable all tasks except TASK_GYRO, TASK_ACCEL
-    for (int taskId = 0; taskId < TASK_COUNT; ++taskId) {
-        setTaskEnabled(static_cast<taskId_e>(taskId), false);
-    }
-    setTaskEnabled(TASK_GYRO, true);
-    setTaskEnabled(TASK_ACCEL, true);
-
-#if defined(USE_TASK_STATISTICS)
-    // set the average run time for TASK_ACCEL
-    tasks[TASK_ACCEL].movingSumExecutionTimeUs = TEST_UPDATE_ACCEL_TIME * TASK_STATS_MOVING_SUM_COUNT;
-#endif
-
-    /* Test that another task will run if there's plenty of time till the next gyro sample time */
-    // set it up so TASK_GYRO just ran and TASK_ACCEL is ready to run
-    simulatedTime = startTime;
-    tasks[TASK_GYRO].lastExecutedAtUs = simulatedTime;
-    tasks[TASK_ACCEL].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(1000);
-    // reset the flags
-    resetGyroTaskTestFlags();
-
-    // run the scheduler
-    scheduler();
-    // the gyro, filter and PID task indicators should be false
-    EXPECT_FALSE(taskGyroRan);
-    EXPECT_FALSE(taskFilterRan);
-    EXPECT_FALSE(taskPidRan);
-    // TASK_ACCEL should have run
-    EXPECT_EQ(&tasks[TASK_ACCEL], unittest_scheduler_selectedTask);
-
-    /* Test that another task won't run if the time till the gyro task is less than the guard interval */
-    // set it up so TASK_GYRO will run soon and TASK_ACCEL is ready to run
-    simulatedTime = startTime;
-    tasks[TASK_GYRO].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(TEST_GYRO_SAMPLE_HZ) + GYRO_TASK_GUARD_INTERVAL_US / 2;
-    tasks[TASK_ACCEL].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(1000);
-    // reset the flags
-    resetGyroTaskTestFlags();
-
-    // run the scheduler
-    scheduler();
-    // the gyro, filter and PID task indicators should be false
-    EXPECT_FALSE(taskGyroRan);
-    EXPECT_FALSE(taskFilterRan);
-    EXPECT_FALSE(taskPidRan);
-    // TASK_ACCEL should not have run
-    EXPECT_EQ(static_cast<task_t*>(0), unittest_scheduler_selectedTask);
-
-    /* Test that another task won't run if the time till the gyro task is less than the average task interval */
-    // set it up so TASK_GYRO will run soon and TASK_ACCEL is ready to run
-    simulatedTime = startTime;
-    tasks[TASK_GYRO].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(TEST_GYRO_SAMPLE_HZ) + TEST_UPDATE_ACCEL_TIME / 2;
-    tasks[TASK_ACCEL].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(1000);
-    // reset the flags
-    resetGyroTaskTestFlags();
-
-    // run the scheduler
-    scheduler();
-    // the gyro, filter and PID task indicators should be false
-    EXPECT_FALSE(taskGyroRan);
-    EXPECT_FALSE(taskFilterRan);
-    EXPECT_FALSE(taskPidRan);
-    // TASK_ACCEL should not have run
-    EXPECT_EQ(static_cast<task_t*>(0), unittest_scheduler_selectedTask);
-
-    /* Test that another task will run if the gyro task gets executed */
-    // set it up so TASK_GYRO will run now and TASK_ACCEL is ready to run
-    simulatedTime = startTime;
-    tasks[TASK_GYRO].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(TEST_GYRO_SAMPLE_HZ);
-    tasks[TASK_ACCEL].lastExecutedAtUs = simulatedTime - TASK_PERIOD_HZ(1000);
-    // reset the flags
-    resetGyroTaskTestFlags();
-
-    // make the TASK_FILTER and TASK_PID ready to run
-    taskFilterReady = true;
-    taskPidReady = true;
-
-    // run the scheduler
-    scheduler();
-    // TASK_GYRO, TASK_FILTER, and TASK_PID should all run
-    EXPECT_TRUE(taskGyroRan);
-    EXPECT_TRUE(taskFilterRan);
-    EXPECT_TRUE(taskPidRan);
-    // TASK_ACCEL should have run
-    EXPECT_EQ(&tasks[TASK_ACCEL], unittest_scheduler_selectedTask);
-}