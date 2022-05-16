a3d989412af46f3936541696a213851f087e2ca7
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-16 12:28:02 +0100

    RTFL: Remove USE_BATTERY_VOLTAGE_SAG_COMPENSATION

--- src/main/flight/mixer_init.c
+++ src/main/flight/mixer_init.c
@@ -304,18 +304,6 @@ void mixerInitProfile(void)
         mixerRuntime.motorOutputLow = DSHOT_MIN_THROTTLE; // Override value set by initEscEndpoints to allow zero motor drive
     }
 #endif
-
-#if defined(USE_BATTERY_VOLTAGE_SAG_COMPENSATION)
-    mixerRuntime.vbatSagCompensationFactor = 0.0f;
-    if (currentPidProfile->vbat_sag_compensation > 0) {
-        //TODO: Make this voltage user configurable
-        mixerRuntime.vbatFull = CELL_VOLTAGE_FULL_CV;
-        mixerRuntime.vbatRangeToCompensate = mixerRuntime.vbatFull - batteryConfig()->vbatwarningcellvoltage;
-        if (mixerRuntime.vbatRangeToCompensate > 0) {
-            mixerRuntime.vbatSagCompensationFactor = ((float)currentPidProfile->vbat_sag_compensation) / 100.0f;
-        }
-    }
-#endif
 }
 
 #ifndef USE_QUAD_MIXER_ONLY
