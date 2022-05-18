92012715a84b5cea2653b7e4917434718928cc1d
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-18 18:13:32 +0100

    RTFL: Remove fpvCamAngle

--- src/main/pg/rx.h
+++ src/main/pg/rx.h
@@ -42,7 +42,6 @@ typedef struct rxConfig_s {
     uint16_t midrc;                            // Some radios have not a neutral point centered on 1500. can be changed here
     uint16_t mincheck;                         // minimum rc end
     uint16_t maxcheck;                         // maximum rc end
-    uint8_t fpvCamAngleDegrees;                // Camera angle to be scaled into rc commands
     uint16_t rx_min_usec;
     uint16_t rx_max_usec;
     uint8_t max_aux_channel;
