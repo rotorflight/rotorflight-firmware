c6cb838016865cfde286a678bbe2163130398fd6
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-05-25 11:03:41 +0100

    RTFL: Refactor servo interface

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -840,7 +840,6 @@ const clivalue_t valueTable[] = {
 // PG_SERVO_CONFIG
 #ifdef USE_SERVOS
     { "servo_pwm_rate",             VAR_UINT16 | MASTER_VALUE, .config.minmaxUnsigned = { 50, 498 }, PG_SERVO_CONFIG, offsetof(servoConfig_t, dev.servoPwmRate) },
-    { "servo_lowpass_hz",           VAR_UINT16 | MASTER_VALUE, .config.minmaxUnsigned = { 0, 400}, PG_SERVO_CONFIG, offsetof(servoConfig_t, servo_lowpass_freq) },
 #endif
 
 // PG_CONTROLRATE_PROFILES
