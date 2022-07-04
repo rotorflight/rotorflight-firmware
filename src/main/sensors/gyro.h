5875b35c959dfc8962becbee6212914a115c6c67
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-07-05 00:23:28 +0100

    RTFL: Remove gyro_lpf_dyn_expo

--- src/main/sensors/gyro.h
+++ src/main/sensors/gyro.h
@@ -116,7 +116,6 @@ typedef struct gyro_s {
     uint8_t dynLpfFilter;
     uint16_t dynLpfMin;
     uint16_t dynLpfMax;
-    uint8_t dynLpfCurveExpo;
 #endif
 
 #ifdef USE_GYRO_OVERFLOW_CHECK
@@ -179,7 +178,6 @@ typedef struct gyroConfig_s {
     uint8_t gyro_filter_debug_axis;
 
     uint8_t gyrosDetected; // What gyros should detection be attempted for on startup. Automatically set on first startup.
-    uint8_t gyro_lpf1_dyn_expo; // set the curve for dynamic gyro lowpass filter
 } gyroConfig_t;
 
 PG_DECLARE(gyroConfig_t, gyroConfig);
