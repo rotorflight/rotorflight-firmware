4b13046304f51e427f8f7255cadb7ba07209c07e
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-09-18 10:31:48 +0100

    MSP: Add MSP_SET_MIXER_INPUT

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -2963,6 +2963,16 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
         // RF TODO mixerConfigMutable()->swash_phase = sbufReadU8(src);
         break;
 
+    case MSP_SET_MIXER_INPUT:
+        i = sbufReadU8(src);
+        if (i >= MIXER_INPUT_COUNT) {
+            return MSP_RESULT_ERROR;
+        }
+        mixerInputsMutable(i)->rate = sbufReadU16(src);
+        mixerInputsMutable(i)->min = sbufReadU16(src);
+        mixerInputsMutable(i)->max = sbufReadU16(src);
+        break;
+
     case MSP_SET_MIXER_RULE:
         i = sbufReadU8(src);
         if (i >= MIXER_RULE_COUNT) {
