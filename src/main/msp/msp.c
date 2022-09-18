ff9c373f2aaa143989f2b7d5b229ac909c788a3b
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-09-18 10:34:50 +0100

    MSP: Add MSP_MIXER_OVERRIDE

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -1476,6 +1476,12 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         }
         break;
 
+    case MSP_MIXER_OVERRIDE:
+        for (int i = 0; i < MIXER_INPUT_COUNT; i++) {
+            sbufWriteU16(dst, mixerGetOverride(i));
+        }
+        break;
+
     case MSP_RX_CONFIG:
         sbufWriteU8(dst, rxConfig()->serialrx_provider);
         sbufWriteU16(dst, rxConfig()->maxcheck);
