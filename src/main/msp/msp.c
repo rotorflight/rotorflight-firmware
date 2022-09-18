809ac0cae744f7582085117874877488829610f6
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-09-18 10:13:03 +0100

    MSP: Add MSP_SET_RPM_FILTER

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -2463,6 +2463,20 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
         gyroInitFilters();
         break;
 
+#ifdef USE_RPM_FILTER
+    case MSP_SET_RPM_FILTER:
+        i = sbufReadU8(src);
+        if (i >= RPM_FILTER_BANK_COUNT) {
+            return MSP_RESULT_ERROR;
+        }
+        rpmFilterConfigMutable()->filter_bank_motor_index[i] = sbufReadU8(src);
+        rpmFilterConfigMutable()->filter_bank_gear_ratio[i] = sbufReadU16(src);
+        rpmFilterConfigMutable()->filter_bank_notch_q[i] = sbufReadU16(src);
+        rpmFilterConfigMutable()->filter_bank_min_hz[i] = sbufReadU16(src);
+        rpmFilterConfigMutable()->filter_bank_max_hz[i] = sbufReadU16(src);
+        break;
+#endif
+
     case MSP_SET_PID_ADVANCED:
         currentPidProfile->error_limit[0] = sbufReadU16(src);
         currentPidProfile->error_limit[1] = sbufReadU16(src);
