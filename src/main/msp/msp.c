b04e4157000d4f44d6c41d6c26431d0b00db6320
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-09-23 11:41:35 +0100

    MSP: Add MSP_SET_DEBUG_CONFIG

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -2153,6 +2153,12 @@ static mspResult_e mspProcessInCommand(mspDescriptor_t srcDesc, int16_t cmdMSP, 
 
         break;
 #endif
+
+    case MSP_SET_DEBUG_CONFIG:
+        systemConfigMutable()->debug_mode = sbufReadU8(src);
+        systemConfigMutable()->debug_axis = sbufReadU8(src);
+        break;
+
     case MSP_SET_ARMING_CONFIG:
         armingConfigMutable()->auto_disarm_delay = sbufReadU8(src);
         sbufReadU8(src); // reserved
