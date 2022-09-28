67fa90b8b61c2b100081be06b4901ee4b2ce2d34
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-09-28 23:43:55 +0100

    WIP: Refactor common/maths

--- src/main/common/maths.h
+++ src/main/common/maths.h
@@ -20,59 +20,249 @@
 
 #pragma once
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+/*
+ * Floating point constants
+ */
+
+#define M_PIf           3.14159265358979323846f
+#define M_PI2f          1.57079632679489661923f
+#define M_2PIf          6.28318530717958647693f
+
+#define M_RADf          0.01745329251994329577f
+#define RAD             M_RADf
+
+
+/*
+ * Fast math routines
+ */
+
+#ifndef USE_STANDARD_MATH
+
+float sin_approx(float x);
+float cos_approx(float x);
+float tan_approx(float x);
+float atan2_approx(float y, float x);
+float asin_approx(float x);
+float acos_approx(float x);
+float exp_approx(float val);
+float log_approx(float val);
+float pow_approx(float a, float b);
+
+#else /* USE_STANDARD_MATH */
+
+#define sin_approx(x)       sinf(x)
+#define cos_approx(x)       cosf(x)
+#define tan_approx(x)       tanf(x)
+#define asin_approx(x)      asinf(x)
+#define acos_approx(x)      acosf(x)
+#define atan2_approx(y,x)   atan2f(y,x)
+#define exp_approx(x)       expf(x)
+#define log_approx(x)       logf(x)
+#define pow_approx(a, b)    powf(b, a)
+
+#endif /* USE_STANDARD_MATH */
+
+
+/*
+ * Unit conversion macros
+ */
+
+#define DEGREES_TO_DECIDEGREES(angle)       ((angle) * 10)
+#define DECIDEGREES_TO_DEGREES(angle)       ((angle) / 10)
+#define DECIDEGREES_TO_RADIANS(angle)       ((angle) / 10 * M_RADf)
+#define DEGREES_TO_RADIANS(angle)           ((angle) * M_RADf)
+
+#define CM_S_TO_KM_H(cmps)                  ((cmps) * 9 / 250)
+#define CM_S_TO_MPH(cmps)                   ((cmps) * 125 / 5588)
+
+#define HZ_TO_INTERVAL(x)                   (1.0f / (x))
+#define HZ_TO_INTERVAL_US(x)                (1000000 / (x))
+
+
+/*
+ * Basic math macros
+ */
 
 #ifndef sq
-#define sq(x) ((x)*(x))
+#define sq(x) POWER2(x)
 #endif
-#define power3(x) ((x)*(x)*(x))
 
-// Undefine this for use libc sinf/cosf. Keep this defined to use fast sin/cos approximations
-#define FAST_MATH             // order 9 approximation
-#define VERY_FAST_MATH        // order 7 approximation
+#define POWER2(x) \
+  __extension__ ({ \
+    __typeof__ (x) _x = (x); \
+    _x * _x; })
 
-// Use floating point M_PI instead explicitly.
-#define M_PIf       3.14159265358979323846f
-#define M_EULERf    2.71828182845904523536f
+#define POWER3(x) \
+  __extension__ ({ \
+    __typeof__ (x) _x = (x); \
+    _x * _x * _x; })
 
-#define RAD    (M_PIf / 180.0f)
-#define DEGREES_TO_DECIDEGREES(angle) ((angle) * 10)
-#define DECIDEGREES_TO_DEGREES(angle) ((angle) / 10)
-#define DECIDEGREES_TO_RADIANS(angle) ((angle) / 10.0f * 0.0174532925f)
-#define DEGREES_TO_RADIANS(angle) ((angle) * 0.0174532925f)
-
-#define CM_S_TO_KM_H(centimetersPerSecond) ((centimetersPerSecond) * 36 / 1000)
-#define CM_S_TO_MPH(centimetersPerSecond) ((centimetersPerSecond) * 10000 / 5080 / 88)
+#define POWER4(x) \
+  __extension__ ({ \
+    __typeof__ (x) _x = (x); \
+    _x * _x * _x * _x; })
 
 #define MIN(a,b) \
-  __extension__ ({ __typeof__ (a) _a = (a); \
-  __typeof__ (b) _b = (b); \
-  _a < _b ? _a : _b; })
+  __extension__ ({ \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a < _b ? _a : _b; })
+
 #define MAX(a,b) \
-  __extension__ ({ __typeof__ (a) _a = (a); \
-  __typeof__ (b) _b = (b); \
-  _a > _b ? _a : _b; })
+  __extension__ ({ \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a > _b ? _a : _b; })
+
 #define ABS(x) \
-  __extension__ ({ __typeof__ (x) _x = (x); \
-  _x > 0 ? _x : -_x; })
-#define SIGN(x) \
-  __extension__ ({ __typeof__ (x) _x = (x); \
-  (_x > 0) - (_x < 0); })
+  __extension__ ({ \
+    __typeof__ (x) _x = (x); \
+    _x > 0 ? _x : -_x; })
 
-#define Q12 (1 << 12)
 
-#define HZ_TO_INTERVAL(x) (1.0f / (x))
-#define HZ_TO_INTERVAL_US(x) (1000000 / (x))
+/*
+ * Basic math operations
+ */
 
-typedef int32_t fix12_t;
+static inline int constrain(int value, int low, int high)
+{
+    if (value < low)
+        return low;
+    else if (value > high)
+        return high;
+    else
+        return value;
+}
+
+static inline float constrainf(float value, float low, float high)
+{
+    if (value < low)
+        return low;
+    else if (value > high)
+        return high;
+    else
+        return value;
+}
+
+static inline int scaleRange(int src, int srcFrom, int srcTo, int dstFrom, int dstTo)
+{
+    const int srcRange = srcTo - srcFrom;
+    const int dstRange = dstTo - dstFrom;
+    return ((src - srcFrom) * dstRange) / srcRange + dstFrom;
+}
+
+static inline float scaleRangef(float src, float srcFrom, float srcTo, float dstFrom, float dstTo)
+{
+    const float srcRange = srcTo - srcFrom;
+    const float dstRange = dstTo - dstFrom;
+    return ((src - srcFrom) * dstRange) / srcRange + dstFrom;
+}
+
+static inline float slewLimit(float current, float target, float rate)
+{
+    if (rate > 0) {
+        if (target > current + rate)
+            return current + rate;
+        if (target < current - rate)
+            return current - rate;
+    }
+    return target;
+}
+
+static inline float slewUpLimit(float current, float target, float rate)
+{
+    if (rate > 0) {
+        if (target > current + rate)
+            return current + rate;
+    }
+    return target;
+}
+
+static inline float slewDownLimit(float current, float target, float rate)
+{
+    if (rate > 0) {
+        if (target < current - rate)
+            return current - rate;
+    }
+    return target;
+}
+
+static inline int32_t applyDeadband(const int32_t value, const int32_t deadband)
+{
+    if (value > deadband)
+        return value - deadband;
+    else if (value < -deadband)
+        return value + deadband;
+    return value;
+}
+
+static inline float fapplyDeadband(const float value, const float deadband)
+{
+    if (value > deadband)
+        return value - deadband;
+    else if (value < -deadband)
+        return value + deadband;
+    return value;
+}
+
+static inline float transition(const float src, const float srcMin, const float srcMax,
+                               const float dstMin, const float dstMax)
+{
+    if (src > srcMax)
+        return dstMax;
+    else if (src < srcMin)
+        return dstMin;
+
+    return scaleRangef(src, srcMin, srcMax, dstMin, dstMax);
+}
+
+static inline float degreesToRadians(int16_t degrees)
+{
+    return DEGREES_TO_RADIANS(degrees);
+}
+
+
+/*
+ * Standard deviation calculus
+ */
 
 typedef struct stdev_s
 {
-    float m_oldM, m_newM, m_oldS, m_newS;
+    float m_oldM;
+    float m_newM;
+    float m_oldS;
+    float m_newS;
     int m_n;
 } stdev_t;
 
-// Floating point 3 vector.
+void devClear(stdev_t *dev);
+void devPush(stdev_t *dev, float x);
+float devVariance(stdev_t *dev);
+float devStandardDeviation(stdev_t *dev);
+
+/*
+ * Median calculus
+ */
+
+int32_t quickMedianFilter3(int32_t * v);
+int32_t quickMedianFilter5(int32_t * v);
+int32_t quickMedianFilter7(int32_t * v);
+int32_t quickMedianFilter9(int32_t * v);
+
+float quickMedianFilter3f(float * v);
+float quickMedianFilter5f(float * v);
+float quickMedianFilter7f(float * v);
+float quickMedianFilter9f(float * v);
+
+
+/*
+ * 3D calculus
+ */
+
 typedef struct fp_vector {
     float X;
     float Y;
@@ -84,8 +274,6 @@ typedef union u_fp_vector {
     t_fp_vector_def V;
 } t_fp_vector;
 
-// Floating point Euler angles.
-// Be carefull, could be either of degrees or radians.
 typedef struct fp_angles {
     float roll;
     float pitch;
@@ -98,88 +286,10 @@ typedef union {
 } fp_angles_t;
 
 typedef struct fp_rotationMatrix_s {
-    float m[3][3];              // matrix
+    float m[3][3];
 } fp_rotationMatrix_t;
-
-int gcd(int num, int denom);
-int32_t applyDeadband(int32_t value, int32_t deadband);
-float fapplyDeadband(float value, float deadband);
-
-void devClear(stdev_t *dev);
-void devPush(stdev_t *dev, float x);
-float devVariance(stdev_t *dev);
-float devStandardDeviation(stdev_t *dev);
-float degreesToRadians(int16_t degrees);
-
-int scaleRange(int x, int srcFrom, int srcTo, int destFrom, int destTo);
-float scaleRangef(float x, float srcFrom, float srcTo, float destFrom, float destTo);
 
 void buildRotationMatrix(fp_angles_t *delta, fp_rotationMatrix_t *rotation);
 void applyMatrixRotation(float *v, fp_rotationMatrix_t *rotationMatrix);
 
-int32_t quickMedianFilter3(int32_t * v);
-int32_t quickMedianFilter5(int32_t * v);
-int32_t quickMedianFilter7(int32_t * v);
-int32_t quickMedianFilter9(int32_t * v);
 
-float quickMedianFilter3f(float * v);
-float quickMedianFilter5f(float * v);
-float quickMedianFilter7f(float * v);
-float quickMedianFilter9f(float * v);
-
-#if defined(FAST_MATH) || defined(VERY_FAST_MATH)
-float sin_approx(float x);
-float cos_approx(float x);
-float atan2_approx(float y, float x);
-float acos_approx(float x);
-#define tan_approx(x)       (sin_approx(x) / cos_approx(x))
-float exp_approx(float val);
-float log_approx(float val);
-float pow_approx(float a, float b);
-#else
-#define sin_approx(x)       sinf(x)
-#define cos_approx(x)       cosf(x)
-#define atan2_approx(y,x)   atan2f(y,x)
-#define acos_approx(x)      acosf(x)
-#define tan_approx(x)       tanf(x)
-#define exp_approx(x)       expf(x)
-#define log_approx(x)       logf(x)
-#define pow_approx(a, b)    powf(b, a)
-#endif
-
-void arraySubInt32(int32_t *dest, int32_t *array1, int32_t *array2, int count);
-
-int16_t qPercent(fix12_t q);
-int16_t qMultiply(fix12_t q, int16_t input);
-fix12_t qConstruct(int16_t num, int16_t den);
-
-static inline int constrain(int amt, int low, int high)
-{
-    if (amt < low)
-        return low;
-    else if (amt > high)
-        return high;
-    else
-        return amt;
-}
-
-static inline float constrainf(float amt, float low, float high)
-{
-    if (amt < low)
-        return low;
-    else if (amt > high)
-        return high;
-    else
-        return amt;
-}
-
-static inline float slew_limit(float current, float target, float rate)
-{
-    if (rate > 0) {
-        if (target > current)
-            return MIN(current + rate, target);
-        else
-            return MAX(current - rate, target);
-    }
-    return target;
-}
