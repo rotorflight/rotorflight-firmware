6ca883230a2ca7728f33330b6269e26d9090a8cd
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-10-05 23:10:09 +0100

    WIP: Add governor blackbox events

--- src/main/blackbox/blackbox.h
+++ src/main/blackbox/blackbox.h
@@ -56,6 +56,7 @@ typedef enum FlightLogEvent {
     FLIGHT_LOG_EVENT_LOGGING_RESUME = 14,
     FLIGHT_LOG_EVENT_DISARM = 15,
     FLIGHT_LOG_EVENT_FLIGHTMODE = 30, // Add new event type for flight mode status.
+    FLIGHT_LOG_EVENT_GOVSTATE = 50,   // Add new event type for main motor governor state.
     FLIGHT_LOG_EVENT_LOG_END = 255
 } FlightLogEvent;
 
