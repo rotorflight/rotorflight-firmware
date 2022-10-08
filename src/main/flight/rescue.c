25a338200951613fd348a853e90d7fe6ce4ab452
Petri Mattila <petri.t.j.mattila@gmail.com>
2022-10-08 23:53:42 +0100

    WIP: Rescue mode start

---
+++ src/main/flight/rescue.c
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+#include "platform.h"
+
+#include "build/build_config.h"
+#include "build/debug.h"
+
+#include "common/filter.h"
+#include "common/maths.h"
+
+#include "config/feature.h"
+#include "config/config.h"
+
+#include "drivers/time.h"
+
+#include "fc/runtime_config.h"
+#include "fc/rc_controls.h"
+
+#include "flight/rescue.h"
+#include "flight/pid.h"
+
+
+
+//// Interface functions
+
+float rescueApply(uint8_t axis, float setpoint)
+{
+    UNUSED(axis);
+    return setpoint;
+}
+
+void rescueUpdate(void)
+{
+
+}
+
+void rescueInitProfile(const pidProfile_t *pidProfile)
+{
+    UNUSED(pidProfile);
+}
+
+void rescueInit(const pidProfile_t *pidProfile)
+{
+    UNUSED(pidProfile);
+}
---
+++ src/main/flight/rescue.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "platform.h"
+
+#include "pg/pg.h"
+
+#include "flight/pid.h"
+
+typedef enum {
+    RESCUE_OFF,
+    RESCUE_INIT_CLIMB,
+    RESCUE_FLIP_OVER,
+    RESCUE_FINAL_CLIMB,
+    RESCUE_STABILISED,
+    RESCUE_ALT_HOLD,
+} rescueState_e;
+
+typedef struct rescueConfig_s {
+    uint8_t  rescue_mode;
+    uint16_t rescue_climb_time;
+    uint16_t recuue_climb_collective;
+} rescueConfig_t;
+
+
+float rescueApply(uint8_t axis, float setpoint);
+
+void rescueUpdate();
+
+void rescueInit(const pidProfile_t *pidProfile);
+void rescueInitProfile(const pidProfile_t *pidProfile);
