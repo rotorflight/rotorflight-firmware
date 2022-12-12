heliholic/devel

WIP: Add collective headspeed correction

src/main/flight/mixer.h
@@ -118,6 +118,8 @@ typedef struct
     uint8_t   swash_ring;       // Swash ring size
     int16_t   swash_phase;      // Swashplate phasing angle
 
+    uint8_t   coll_hs_precomp;  // Collective correction vs. headspeed
+
 } mixerConfig_t;
 
 PG_DECLARE(mixerConfig_t, mixerConfig);

rotorflight/rotorflight-firmware

src/main/flight/mixer.h
@@ -118,6 +118,8 @@ typedef struct
     uint8_t   swash_ring;       // Swash ring size
     int16_t   swash_phase;      // Swashplate phasing angle
 
+    uint8_t   coll_correction;  // Collective correction vs. headspeed
+
 } mixerConfig_t;
 
 PG_DECLARE(mixerConfig_t, mixerConfig);