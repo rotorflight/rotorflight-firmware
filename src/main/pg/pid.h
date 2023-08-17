heliholic/devel

Add cyclic crosstalk precomp (pitch-to-roll)

src/main/pg/pid.h
@@ -165,6 +165,10 @@ typedef struct pidProfile_s {
 
     uint8_t             pitch_collective_ff_gain;
 
+    uint8_t             cyclic_crosstalk_mode;
+    int16_t             cyclic_crosstalk_gain;
+    uint8_t             cyclic_crosstalk_cutoff;
+
     pidAngleMode_t      angle;
     pidHorizonMode_t    horizon;
     pidTrainerMode_t    trainer;

rotorflight/rotorflight-firmware

src/main/pg/pid.h
@@ -151,6 +151,10 @@ typedef struct pidProfile_s {
 
     uint8_t             pitch_collective_ff_gain;
 
+    uint8_t             cyclic_crosstalk_mode;
+    uint8_t             cyclic_crosstalk_gain;
+    uint8_t             cyclic_crosstalk_cutoff;
+
     pidAngleMode_t      angle;
     pidHorizonMode_t    horizon;
     pidTrainerMode_t    trainer;