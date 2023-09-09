8ea4a63e84b74003fecaa42d82871f1f3f27d61c
Petri Mattila <petri.t.j.mattila@gmail.com>
2023-09-09 12:25:54 +0100

    Fix flashfs asynchronous erase

--- src/main/fc/tasks.c
+++ src/main/fc/tasks.c
@@ -60,6 +60,7 @@
 #include "io/asyncfatfs/asyncfatfs.h"
 #include "io/beeper.h"
 #include "io/dashboard.h"
+#include "io/flashfs.h"
 #include "io/gps.h"
 #include "io/ledstrip.h"
 #include "io/piniobox.h"
@@ -123,6 +124,10 @@ static void taskMain(timeUs_t currentTimeUs)
 
 #ifdef USE_SDCARD
     afatfs_poll();
+#endif
+
+#ifdef USE_FLASHFS
+    flashfsEraseAsync();
 #endif
 }
 
