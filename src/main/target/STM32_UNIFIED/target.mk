46f497ff7bdf0f67f0af1190de74ae3f422bcd9c
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-01-01 19:02:12 +0000

    Clean up unified target config

--- src/main/target/STM32_UNIFIED/target.mk
+++ src/main/target/STM32_UNIFIED/target.mk
@@ -35,27 +35,3 @@ TARGET_SRC = \
     $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
     $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
     $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
-    rx/cc2500_common.c \
-    rx/cc2500_frsky_shared.c \
-    rx/cc2500_frsky_d.c \
-    rx/cc2500_frsky_x.c \
-    rx/cc2500_sfhss.c \
-    rx/cc2500_redpine.c \
-    rx/a7105_flysky.c \
-    rx/cyrf6936_spektrum.c \
-    drivers/rx/expresslrs_driver.c \
-    rx/expresslrs.c \
-    rx/expresslrs_common.c \
-    rx/expresslrs_telemetry.c \
-    drivers/rx/rx_cc2500.c \
-    drivers/rx/rx_a7105.c \
-    drivers/rx/rx_cyrf6936.c \
-    drivers/rx/rx_sx127x.c \
-    drivers/rx/rx_sx1280.c \
-
-ifneq ($(findstring _OSD,$(TARGET)),)
-VARIANT_SRC = \
-    drivers/max7456.c \
-    drivers/vtx_rtc6705.c \
-    drivers/vtx_rtc6705_soft_spi.c
-endif
