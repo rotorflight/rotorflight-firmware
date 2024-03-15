a1d7b164ca43bd765eca77beacd5e2e915c1d2e8
Rob Gayle <bob01@rogers.com>
2024-03-15 11:52:55 -0400

    OpenYGE - support v3 frame header extension - documentation
    (cherry picked from commit 00fc315409dd568015c3bb77abaf3bc157c9b503)

--- src/main/sensors/esc_sensor.c
+++ src/main/sensors/esc_sensor.c
@@ -1545,28 +1545,30 @@ static void apdSensorProcess(timeUs_t currentTimeUs)
  *     1:       version;            // frame version
  *     2:       frame_type          // telemetry data = 0
  *     3:       frame_length;       // frame length including header and CRC
+ *     4:       reserved            // reserved
+ *     5:       reserved            // reserved
  *
  * Payload...
- *     4:       reserved            // reserved
- *     5:       temperature;        // C degrees (0-> -40°C, 255->215°C)
- *   6,7:       voltage;            // 0.01V    Little endian!
- *   8,9:       current;            // 0.01A    Little endian!
- * 10,11:       consumption;        // mAh      Little endian!
- * 12,13:       rpm;                // 0.1rpm   Little endian!
- *    14:       pwm;                // %
- *    15:       throttle;           // %
- * 16,17:       bec_voltage;        // 0.01V    Little endian!
- * 18,19:       bec_current;        // 0.01A    Little endian!
- *    20:       bec_temp;           // C degrees (0-> -40°C, 255->215°C)
- *    21:       status1;            // see documentation
- *    22:       cap_temp;           // C degrees (0-> -40°C, 255->215°C)
- *    23:       aux_temp;           // C degrees (0-> -40°C, 255->215°C)
- *    24:       status2;            // reserved
- *    25:       reserved1;          // reserved
- * 26,27:       idx;                // maybe future use
- * 28,29:       idx_data;           // maybe future use
+ *    +0:       reserved            // reserved
+ *     1:       temperature;        // C degrees (0-> -40°C, 255->215°C)
+ *   2,3:       voltage;            // 0.01V    Little endian!
+ *   4,5:       current;            // 0.01A    Little endian!
+ *   6,7:       consumption;        // mAh      Little endian!
+ *   8,9:       rpm;                // 0.1rpm   Little endian!
+ *    10:       pwm;                // %
+ *    11:       throttle;           // %
+ * 12,13:       bec_voltage;        // 0.01V    Little endian!
+ * 14,15:       bec_current;        // 0.01A    Little endian!
+ *    16:       bec_temp;           // C degrees (0-> -40°C, 255->215°C)
+ *    17:       status1;            // see documentation
+ *    18:       cap_temp;           // C degrees (0-> -40°C, 255->215°C)
+ *    19:       aux_temp;           // C degrees (0-> -40°C, 255->215°C)
+ *    20:       status2;            // reserved
+ *    21:       reserved1;          // reserved
+ * 22,23:       idx;                // maybe future use
+ * 24,25:       idx_data;           // maybe future use
  *
- * 30,31:       crc16;              // CCITT, poly: 0x1021
+ * 32,33:       crc16;              // CCITT, poly: 0x1021
  *
  */
 
