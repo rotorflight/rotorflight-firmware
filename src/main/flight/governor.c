f29df162853db7ff5f42c3bec75e84a359dfb8c8
Rotorflight <rotorflightfc@gmail.com>
2024-03-28 18:39:18 +0000

    Fix TTA headroom calculation in Gov Passthrough (#92)
    Co-authored-by: Petri Mattila <petri.t.j.mattila@gmail.com>

--- src/main/flight/governor.c
+++ src/main/flight/governor.c
@@ -465,7 +465,13 @@ static void govUpdateData(void)
     if (mixerMotorizedTail() && gov.TTAGain != 0) {
         float YAW = mixerGetInput(MIXER_IN_STABILIZED_YAW);
         float TTA = filterApply(&gov.TTAFilter, YAW) * getSpoolUpRatio() * gov.TTAGain;
-        float headroom = 2 * fmaxf(gov.TTALimit - gov.fullHeadSpeedRatio, 0);
+        float headroom = 0;
+
+        if (gov.mode > GM_PASSTHROUGH)
+            headroom = 2 * fmaxf(1.0f + gov.TTALimit - gov.fullHeadSpeedRatio, 0);
+        else
+            headroom = gov.TTALimit;
+
         gov.TTAAdd = constrainf(TTA, 0, headroom);
 
         DEBUG(TTA, 0, YAW * 1000);
@@ -1009,7 +1015,7 @@ void governorInitProfile(const pidProfile_t *pidProfile)
         gov.Kf = pidProfile->governor.f_gain / 100.0f;
 
         gov.TTAGain   = mixerRotationSign() * pidProfile->governor.tta_gain / -125.0f;
-        gov.TTALimit  = pidProfile->governor.tta_limit / 100.0f + 1.0f;
+        gov.TTALimit  = pidProfile->governor.tta_limit / 100.0f;
 
         if (gov.mode >= GM_STANDARD)
             gov.TTAGain /= gov.K * gov.Kp;
