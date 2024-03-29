d23d6c471321fb4a8266ff35a4f4f6d93e01b0ca
Rob Gayle <bob01@rogers.com>
2024-03-29 08:22:45 -0400

    OpenYGE - fix: address possible frame underflow condition, add 2nd paranoid frame length validation outside ISR/callback, declare globals shared w/ callback volatile

--- src/main/sensors/esc_sensor.c
+++ src/main/sensors/esc_sensor.c
@@ -133,8 +133,8 @@ static uint8_t buffer[TELEMETRY_BUFFER_SIZE] = { 0, };
 static volatile uint8_t bufferSize = 0;
 static volatile uint8_t bufferPos = 0;
 
-static uint8_t  readBytes = 0;
-static uint8_t  readIngoreBytes = 0;
+static volatile uint8_t readBytes = 0;
+static volatile uint8_t readIngoreBytes = 0;
 static uint32_t syncCount = 0;
 
 static uint8_t reqLength = 0;
@@ -1547,7 +1547,7 @@ static uint16_t rrfsmBootDelayMs = 0;
 static uint8_t rrfsmMinFrameLength = 0;
 
 static uint32_t rrfsmFrameTimestamp = 0;
-static uint8_t rrfsmFrameLength = 0;
+static volatile uint8_t rrfsmFrameLength = 0;
 static uint16_t rrfsmFramePeriod = 0;
 static uint16_t rrfsmFrameTimeout = 0;
 
@@ -2283,7 +2283,6 @@ typedef struct {
     uint16_t param;                     // parameter value
 } OpenYGEControlFrame_t;
 
-
 static uint16_t oygeAutoFrameTimeout = OPENYGE_AUTO_INITIAL_FRAME_TIMEOUT;
 static uint64_t oygeCachedParams = 0;
 static uint64_t oygeDirtyParams = 0;
@@ -2449,11 +2448,27 @@ static void oygeDecodeTelemetryFrame(void)
     DEBUG(ESC_SENSOR_DATA, DEBUG_DATA_AGE, 0);
 }
 
+static const OpenYGEHeader_t *oygeGetHeaderWithCrcCheck()
+{
+    // get header (w/ paranoid buffer access)
+    const OpenYGEHeader_t *hdr = (OpenYGEHeader_t*)buffer;
+    const uint8_t len = hdr->frame_length;
+    if (len < OPENYGE_MIN_FRAME_LENGTH || len > TELEMETRY_BUFFER_SIZE)
+        return NULL;
+
+    // check CRC
+    const uint16_t crc = buffer[len - 1] << 8 | buffer[len - 2];
+    if (oygeCalculateCRC16_CCITT(buffer, len - 2) != crc)
+        return NULL;
+
+    return hdr;
+}
+
 static bool oygeDecodeAuto(timeMs_t currentTimeMs)
 {
-    // verify CRC16 checksum
-    const uint16_t crc = buffer[rrfsmFrameLength - 1] << 8 | buffer[rrfsmFrameLength - 2];
-    if (oygeCalculateCRC16_CCITT(buffer, rrfsmFrameLength - 2) != crc)
+    // get header w/ CRC check
+    const OpenYGEHeader_t *hdr = oygeGetHeaderWithCrcCheck();
+    if (hdr == NULL)
         return false;
         
     // decode payload
@@ -2493,12 +2508,11 @@ static bool oygeDecodeTelemetry(const OpenYGEHeader_t *hdr, timeMs_t currentTime
 
 static bool oygeDecode(timeMs_t currentTimeMs)
 {
-    // verify CRC16 checksum
-    const uint16_t crc = buffer[rrfsmFrameLength - 1] << 8 | buffer[rrfsmFrameLength - 2];
-    if (oygeCalculateCRC16_CCITT(buffer, rrfsmFrameLength - 2) != crc)
+    // get header w/ CRC check
+    const OpenYGEHeader_t *hdr = oygeGetHeaderWithCrcCheck();
+    if (hdr == NULL)
         return false;
 
-    const OpenYGEHeader_t *hdr = (OpenYGEHeader_t*)buffer;
     switch (hdr->frame_type) {
         case OPENYGE_FTYPE_TELE_AUTO:
         case OPENYGE_FTYPE_TELE_RESP:
@@ -2531,8 +2545,8 @@ static int8_t oygeAccept(uint16_t c)
     }
     else if (readBytes == 4) {
         // frame length
-        if (c > TELEMETRY_BUFFER_SIZE) {
-            // protect against buffer overflow
+        if (c < OPENYGE_MIN_FRAME_LENGTH || c > TELEMETRY_BUFFER_SIZE) {
+            // protect against buffer underflow/overflow
             return -1;
         }
         else {
