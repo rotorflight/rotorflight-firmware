0d32308e5789693ee336b77455502724ab7b90e2
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-06-04 09:49:13 +0100

    Move mixer config to pg/mixer.c

---
+++ src/main/pg/mixer.c
@@ -0,0 +1,60 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "types.h"
+#include "platform.h"
+
+#include "pg/pg_ids.h"
+#include "pg/mixer.h"
+
+#include "config/config_reset.h"
+
+
+PG_REGISTER_WITH_RESET_TEMPLATE(mixerConfig_t, mixerConfig, PG_GENERIC_MIXER_CONFIG, 0);
+
+PG_RESET_TEMPLATE(mixerConfig_t, mixerConfig,
+    .main_rotor_dir = DIR_CW,
+    .tail_rotor_mode = TAIL_MODE_VARIABLE,
+    .tail_motor_idle = 0,
+    .tail_center_trim = 0,
+    .swash_type = SWASH_TYPE_120,
+    .swash_ring = 100,
+    .swash_phase = 0,
+    .swash_pitch_limit = 0,
+    .swash_trim = { 0, 0, 0 },
+    .swash_tta_precomp = 0,
+    .swash_geo_correction = 0,
+);
+
+PG_REGISTER_ARRAY(mixerRule_t, MIXER_RULE_COUNT, mixerRules, PG_GENERIC_MIXER_RULES, 0);
+
+PG_REGISTER_ARRAY_WITH_RESET_FN(mixerInput_t, MIXER_INPUT_COUNT, mixerInputs, PG_GENERIC_MIXER_INPUTS, 0);
+
+void pgResetFn_mixerInputs(mixerInput_t *input)
+{
+    for (int i = MIXER_IN_STABILIZED_ROLL; i < MIXER_IN_COUNT; i++) {
+        input[i].rate =  1000;
+        input[i].min  = -1000;
+        input[i].max  =  1000;
+    }
+
+    input[MIXER_IN_STABILIZED_THROTTLE].rate =  1000;
+    input[MIXER_IN_STABILIZED_THROTTLE].min  =  0;
+    input[MIXER_IN_STABILIZED_THROTTLE].max  =  1000;
+}
+
+
---
+++ src/main/pg/mixer.h
@@ -0,0 +1,134 @@
+/*
+ * This file is part of Rotorflight.
+ *
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "types.h"
+#include "platform.h"
+
+#include "pg/pg.h"
+
+enum {
+    SWASH_TYPE_NONE = 0,
+    SWASH_TYPE_THRU,
+    SWASH_TYPE_120,
+    SWASH_TYPE_135,
+    SWASH_TYPE_140,
+    SWASH_TYPE_90L,
+    SWASH_TYPE_90V,
+};
+
+enum {
+    TAIL_MODE_VARIABLE,
+    TAIL_MODE_MOTORIZED,
+    TAIL_MODE_BIDIRECTIONAL,
+};
+
+enum {
+    MIXER_IN_NONE = 0,
+    MIXER_IN_STABILIZED_ROLL,
+    MIXER_IN_STABILIZED_PITCH,
+    MIXER_IN_STABILIZED_YAW,
+    MIXER_IN_STABILIZED_COLLECTIVE,
+    MIXER_IN_STABILIZED_THROTTLE,
+    MIXER_IN_RC_COMMAND_ROLL,
+    MIXER_IN_RC_COMMAND_PITCH,
+    MIXER_IN_RC_COMMAND_YAW,
+    MIXER_IN_RC_COMMAND_COLLECTIVE,
+    MIXER_IN_RC_COMMAND_THROTTLE,
+    MIXER_IN_RC_CHANNEL_ROLL,
+    MIXER_IN_RC_CHANNEL_PITCH,
+    MIXER_IN_RC_CHANNEL_YAW,
+    MIXER_IN_RC_CHANNEL_COLLECTIVE,
+    MIXER_IN_RC_CHANNEL_THROTTLE,
+    MIXER_IN_RC_CHANNEL_AUX1,
+    MIXER_IN_RC_CHANNEL_AUX2,
+    MIXER_IN_RC_CHANNEL_AUX3,
+    MIXER_IN_RC_CHANNEL_9,
+    MIXER_IN_RC_CHANNEL_10,
+    MIXER_IN_RC_CHANNEL_11,
+    MIXER_IN_RC_CHANNEL_12,
+    MIXER_IN_RC_CHANNEL_13,
+    MIXER_IN_RC_CHANNEL_14,
+    MIXER_IN_RC_CHANNEL_15,
+    MIXER_IN_RC_CHANNEL_16,
+    MIXER_IN_RC_CHANNEL_17,
+    MIXER_IN_RC_CHANNEL_18,
+    MIXER_IN_COUNT
+};
+
+enum {
+    MIXER_OP_NUL = 0,
+    MIXER_OP_SET,
+    MIXER_OP_ADD,
+    MIXER_OP_MUL,
+    MIXER_OP_COUNT
+};
+
+enum {
+    DIR_CW,
+    DIR_CCW,
+};
+
+
+#define MIXER_RULE_COUNT      32
+#define MIXER_INPUT_COUNT     MIXER_IN_COUNT
+
+typedef struct
+{
+    uint8_t   main_rotor_dir;       // Main rotor direction: CW/CCW
+
+    uint8_t   tail_rotor_mode;      // Tail motor vs. variable pitch tail
+    uint8_t   tail_motor_idle;      // Idle throttle for tail motor
+    int16_t   tail_center_trim;     // Tail center position offset
+
+    uint8_t   swash_type;           // Swashplate type
+    uint8_t   swash_ring;           // Swash ring size
+
+    int16_t   swash_phase;          // Swashplate phasing angle
+    uint16_t  swash_pitch_limit;    // Maximum main rotor blade pitch
+
+    int16_t   swash_trim[3];        // Swash center position trims
+
+    uint8_t   swash_tta_precomp;    // TTA correction %
+
+    int8_t    swash_geo_correction; // Head geometry correction (collective assymetry)
+
+} mixerConfig_t;
+
+PG_DECLARE(mixerConfig_t, mixerConfig);
+
+typedef struct
+{
+    int16_t   rate;             // multiplier
+    int16_t   min;              // minimum input value
+    int16_t   max;              // maximum input value
+} mixerInput_t;
+
+PG_DECLARE_ARRAY(mixerInput_t, MIXER_INPUT_COUNT, mixerInputs);
+
+typedef struct
+{
+    uint8_t   oper;             // rule operation
+    uint8_t   input;            // input channel
+    uint8_t   output;           // output channel
+    int16_t   offset;           // addition
+    int16_t   weight;           // multiplier (weight and direction)
+} mixerRule_t;
+
+PG_DECLARE_ARRAY(mixerRule_t, MIXER_RULE_COUNT, mixerRules);
+
