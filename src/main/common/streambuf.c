heliholic/devel

Refactor streambuf

src/main/common/streambuf.c
@@ -1,27 +1,26 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of Rotorflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <string.h>
 #include <stdint.h>
 
 #include "platform.h"
+
+#include "common/maths.h"
 
 #include "streambuf.h"
 
@@ -32,43 +31,83 @@ sbuf_t *sbufInit(sbuf_t *sbuf, uint8_t *ptr, uint8_t *end)
     return sbuf;
 }
 
+#define SBUFPUSH(dst, val)      ((*(dst)->ptr++ = (val)))
+#define SBUFPOP(src)            ((*(src)->ptr++))
+
 void sbufWriteU8(sbuf_t *dst, uint8_t val)
 {
-    *dst->ptr++ = val;
+    SBUFPUSH(dst,val);
 }
 
 void sbufWriteU16(sbuf_t *dst, uint16_t val)
 {
-    sbufWriteU8(dst, val >> 0);
-    sbufWriteU8(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+}
+
+void sbufWriteU16BE(sbuf_t *dst, uint16_t val)
+{
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
+}
+
+void sbufWriteU24(sbuf_t *dst, uint32_t val)
+{
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 16);
+}
+
+void sbufWriteU24BE(sbuf_t *dst, uint32_t val)
+{
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
 }
 
 void sbufWriteU32(sbuf_t *dst, uint32_t val)
 {
-    sbufWriteU8(dst, val >> 0);
-    sbufWriteU8(dst, val >> 8);
-    sbufWriteU8(dst, val >> 16);
-    sbufWriteU8(dst, val >> 24);
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 24);
+}
+
+void sbufWriteU32BE(sbuf_t *dst, uint32_t val)
+{
+    SBUFPUSH(dst, val >> 24);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
 }
 
 void sbufWriteU64(sbuf_t *dst, uint64_t val)
 {
-    sbufWriteU32(dst, (uint32_t)val);
-    sbufWriteU32(dst, (uint32_t)(val >> 32));
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 24);
+    SBUFPUSH(dst, val >> 32);
+    SBUFPUSH(dst, val >> 40);
+    SBUFPUSH(dst, val >> 48);
+    SBUFPUSH(dst, val >> 56);
 }
 
-void sbufWriteU16BigEndian(sbuf_t *dst, uint16_t val)
+void sbufWriteU64BE(sbuf_t *dst, uint64_t val)
 {
-    sbufWriteU8(dst, val >> 8);
-    sbufWriteU8(dst, (uint8_t)val);
+    SBUFPUSH(dst, val >> 56);
+    SBUFPUSH(dst, val >> 48);
+    SBUFPUSH(dst, val >> 40);
+    SBUFPUSH(dst, val >> 32);
+    SBUFPUSH(dst, val >> 24);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
 }
 
-void sbufWriteU32BigEndian(sbuf_t *dst, uint32_t val)
+void sbufWriteFloat(sbuf_t *dst, float val)
 {
-    sbufWriteU8(dst, val >> 24);
-    sbufWriteU8(dst, val >> 16);
-    sbufWriteU8(dst, val >> 8);
-    sbufWriteU8(dst, (uint8_t)val);
+    sbufWriteU32(dst, REINTERPRET_CAST(val, uint32_t));
 }
 
 
@@ -86,71 +125,95 @@ void sbufWriteData(sbuf_t *dst, const void *data, int len)
 
 void sbufWriteString(sbuf_t *dst, const char *string)
 {
-    sbufWriteData(dst, string, strlen(string));
+    while (*string) { SBUFPUSH(dst, *string++); }
 }
 
 void sbufWriteStringWithZeroTerminator(sbuf_t *dst, const char *string)
 {
-    sbufWriteData(dst, string, strlen(string) + 1);
+    do { SBUFPUSH(dst, *string); } while (*string++);
 }
 
 uint8_t sbufReadU8(sbuf_t *src)
 {
-    return *src->ptr++;
+    return SBUFPOP(src);
 }
 
 uint16_t sbufReadU16(sbuf_t *src)
 {
-    uint16_t ret;
-    ret = sbufReadU8(src);
-    ret |= sbufReadU8(src) << 8;
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 0;
+    ret |= SBUFPOP(src) << 8;
+    return ret;
+}
+
+uint16_t sbufReadU16BE(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 8;
+    ret |= SBUFPOP(src) << 0;
+    return ret;
+}
+
+uint32_t sbufReadU24(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 0;
+    ret |= SBUFPOP(src) << 8;
+    ret |= SBUFPOP(src) << 16;
+    return ret;
+}
+
+uint32_t sbufReadU24BE(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 16;
+    ret |= SBUFPOP(src) << 8;
+    ret |= SBUFPOP(src) << 0;
     return ret;
 }
 
 uint32_t sbufReadU32(sbuf_t *src)
 {
     uint32_t ret;
-    ret = sbufReadU8(src);
-    ret |= sbufReadU8(src) <<  8;
-    ret |= sbufReadU8(src) << 16;
-    ret |= sbufReadU8(src) << 24;
+    ret  = SBUFPOP(src) <<  0;
+    ret |= SBUFPOP(src) <<  8;
+    ret |= SBUFPOP(src) << 16;
+    ret |= SBUFPOP(src) << 24;
+    return ret;
+}
+
+uint32_t sbufReadU32BE(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 24;
+    ret |= SBUFPOP(src) << 16;
+    ret |= SBUFPOP(src) <<  8;
+    ret |= SBUFPOP(src) <<  0;
     return ret;
 }
 
 uint64_t sbufReadU64(sbuf_t *src)
 {
-    uint32_t ret = sbufReadU32(src);
-    return (uint64_t)sbufReadU32(src) << 32 | ret;
+    uint64_t a = sbufReadU32(src);
+    uint64_t b = sbufReadU32(src);
+    return a | (b << 32);
+}
+
+uint64_t sbufReadU64BE(sbuf_t *src)
+{
+    uint64_t a = sbufReadU32(src);
+    uint64_t b = sbufReadU32(src);
+    return (a << 32) | b;
+}
+
+float sbufReadFloat(sbuf_t *src)
+{
+    return REINTERPRET_CAST(sbufReadU32(src), float);
 }
 
 void sbufReadData(sbuf_t *src, void *data, int len)
 {
     memcpy(data, src->ptr, len);
-}
-
-// reader - return bytes remaining in buffer
-// writer - return available space
-int sbufBytesRemaining(sbuf_t *buf)
-{
-    return buf->end - buf->ptr;
-}
-
-uint8_t* sbufPtr(sbuf_t *buf)
-{
-    return buf->ptr;
-}
-
-const uint8_t* sbufConstPtr(const sbuf_t *buf)
-{
-    return buf->ptr;
-}
-
-// advance buffer pointer
-// reader - skip data
-// writer - commit written data
-void sbufAdvance(sbuf_t *buf, int size)
-{
-    buf->ptr += size;
 }
 
 // modifies streambuf so that written data are prepared for reading

rotorflight/rotorflight-firmware

src/main/common/streambuf.c
@@ -1,27 +1,26 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of Rotorflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Rotorflight is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Rotorflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this software.
- *
- * If not, see <http://www.gnu.org/licenses/>.
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <string.h>
 #include <stdint.h>
 
 #include "platform.h"
+
+#include "common/utils.h"
 
 #include "streambuf.h"
 
@@ -32,43 +31,83 @@ sbuf_t *sbufInit(sbuf_t *sbuf, uint8_t *ptr, uint8_t *end)
     return sbuf;
 }
 
+#define SBUFPUSH(dst, val)      ((*(dst)->ptr++ = (val)))
+#define SBUFPOP(src)            ((*(src)->ptr++))
+
 void sbufWriteU8(sbuf_t *dst, uint8_t val)
 {
-    *dst->ptr++ = val;
+    SBUFPUSH(dst,val);
 }
 
 void sbufWriteU16(sbuf_t *dst, uint16_t val)
 {
-    sbufWriteU8(dst, val >> 0);
-    sbufWriteU8(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+}
+
+void sbufWriteU16BE(sbuf_t *dst, uint16_t val)
+{
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
+}
+
+void sbufWriteU24(sbuf_t *dst, uint32_t val)
+{
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 16);
+}
+
+void sbufWriteU24BE(sbuf_t *dst, uint32_t val)
+{
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
 }
 
 void sbufWriteU32(sbuf_t *dst, uint32_t val)
 {
-    sbufWriteU8(dst, val >> 0);
-    sbufWriteU8(dst, val >> 8);
-    sbufWriteU8(dst, val >> 16);
-    sbufWriteU8(dst, val >> 24);
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 24);
+}
+
+void sbufWriteU32BE(sbuf_t *dst, uint32_t val)
+{
+    SBUFPUSH(dst, val >> 24);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
 }
 
 void sbufWriteU64(sbuf_t *dst, uint64_t val)
 {
-    sbufWriteU32(dst, (uint32_t)val);
-    sbufWriteU32(dst, (uint32_t)(val >> 32));
+    SBUFPUSH(dst, val >> 0);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 24);
+    SBUFPUSH(dst, val >> 32);
+    SBUFPUSH(dst, val >> 40);
+    SBUFPUSH(dst, val >> 48);
+    SBUFPUSH(dst, val >> 56);
 }
 
-void sbufWriteU16BigEndian(sbuf_t *dst, uint16_t val)
+void sbufWriteU64BE(sbuf_t *dst, uint64_t val)
 {
-    sbufWriteU8(dst, val >> 8);
-    sbufWriteU8(dst, (uint8_t)val);
+    SBUFPUSH(dst, val >> 56);
+    SBUFPUSH(dst, val >> 48);
+    SBUFPUSH(dst, val >> 40);
+    SBUFPUSH(dst, val >> 32);
+    SBUFPUSH(dst, val >> 24);
+    SBUFPUSH(dst, val >> 16);
+    SBUFPUSH(dst, val >> 8);
+    SBUFPUSH(dst, val >> 0);
 }
 
-void sbufWriteU32BigEndian(sbuf_t *dst, uint32_t val)
+void sbufWriteFloat(sbuf_t *dst, float val)
 {
-    sbufWriteU8(dst, val >> 24);
-    sbufWriteU8(dst, val >> 16);
-    sbufWriteU8(dst, val >> 8);
-    sbufWriteU8(dst, (uint8_t)val);
+    sbufWriteU32(dst, REINTERPRET_CAST(val, uint32_t));
 }
 
 
@@ -86,71 +125,95 @@ void sbufWriteData(sbuf_t *dst, const void *data, int len)
 
 void sbufWriteString(sbuf_t *dst, const char *string)
 {
-    sbufWriteData(dst, string, strlen(string));
+    while (*string) { SBUFPUSH(dst, *string++); }
 }
 
 void sbufWriteStringWithZeroTerminator(sbuf_t *dst, const char *string)
 {
-    sbufWriteData(dst, string, strlen(string) + 1);
+    do { SBUFPUSH(dst, *string); } while (*string++);
 }
 
 uint8_t sbufReadU8(sbuf_t *src)
 {
-    return *src->ptr++;
+    return SBUFPOP(src);
 }
 
 uint16_t sbufReadU16(sbuf_t *src)
 {
-    uint16_t ret;
-    ret = sbufReadU8(src);
-    ret |= sbufReadU8(src) << 8;
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 0;
+    ret |= SBUFPOP(src) << 8;
+    return ret;
+}
+
+uint16_t sbufReadU16BE(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 8;
+    ret |= SBUFPOP(src) << 0;
+    return ret;
+}
+
+uint32_t sbufReadU24(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 0;
+    ret |= SBUFPOP(src) << 8;
+    ret |= SBUFPOP(src) << 16;
+    return ret;
+}
+
+uint32_t sbufReadU24BE(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 16;
+    ret |= SBUFPOP(src) << 8;
+    ret |= SBUFPOP(src) << 0;
     return ret;
 }
 
 uint32_t sbufReadU32(sbuf_t *src)
 {
     uint32_t ret;
-    ret = sbufReadU8(src);
-    ret |= sbufReadU8(src) <<  8;
-    ret |= sbufReadU8(src) << 16;
-    ret |= sbufReadU8(src) << 24;
+    ret  = SBUFPOP(src) <<  0;
+    ret |= SBUFPOP(src) <<  8;
+    ret |= SBUFPOP(src) << 16;
+    ret |= SBUFPOP(src) << 24;
+    return ret;
+}
+
+uint32_t sbufReadU32BE(sbuf_t *src)
+{
+    uint32_t ret;
+    ret  = SBUFPOP(src) << 24;
+    ret |= SBUFPOP(src) << 16;
+    ret |= SBUFPOP(src) <<  8;
+    ret |= SBUFPOP(src) <<  0;
     return ret;
 }
 
 uint64_t sbufReadU64(sbuf_t *src)
 {
-    uint32_t ret = sbufReadU32(src);
-    return (uint64_t)sbufReadU32(src) << 32 | ret;
+    uint64_t a = sbufReadU32(src);
+    uint64_t b = sbufReadU32(src);
+    return a | (b << 32);
+}
+
+uint64_t sbufReadU64BE(sbuf_t *src)
+{
+    uint64_t a = sbufReadU32(src);
+    uint64_t b = sbufReadU32(src);
+    return (a << 32) | b;
+}
+
+float sbufReadFloat(sbuf_t *src)
+{
+    return REINTERPRET_CAST(sbufReadU32(src), float);
 }
 
 void sbufReadData(sbuf_t *src, void *data, int len)
 {
     memcpy(data, src->ptr, len);
-}
-
-// reader - return bytes remaining in buffer
-// writer - return available space
-int sbufBytesRemaining(sbuf_t *buf)
-{
-    return buf->end - buf->ptr;
-}
-
-uint8_t* sbufPtr(sbuf_t *buf)
-{
-    return buf->ptr;
-}
-
-const uint8_t* sbufConstPtr(const sbuf_t *buf)
-{
-    return buf->ptr;
-}
-
-// advance buffer pointer
-// reader - skip data
-// writer - commit written data
-void sbufAdvance(sbuf_t *buf, int size)
-{
-    buf->ptr += size;
 }
 
 // modifies streambuf so that written data are prepared for reading