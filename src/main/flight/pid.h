5daf86685268980da97c010994a8f19cd553f8d1
Petri Mattila <petri.t.j.mattila@gmail.com>
2025-02-24 11:18:51 +0000

    Remove dterm_mode

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -95,8 +95,6 @@ typedef struct pid_s {
     float freq;
 
     uint8_t pidMode;
-    uint8_t dtermMode;
-    uint8_t dtermModeYaw;
 
     uint8_t itermRelaxType;
     uint8_t itermRelaxLevel[PID_AXIS_COUNT];
