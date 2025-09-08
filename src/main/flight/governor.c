heliholic/devel

Refactor Adjustment Functions

src/main/flight/governor.c
@@ -34,6 +34,7 @@
 #include "drivers/time.h"
 
 #include "fc/runtime_config.h"
+#include "fc/rc_adjustments.h"
 #include "fc/rc_controls.h"
 #include "fc/rc.h"
 
@@ -227,7 +228,6 @@ static float govMode2Control(void);
 
 static void governorUpdateState(void);
 static void governorUpdatePassthrough(void);
-
 
 
 //// Access functions
@@ -1009,6 +1009,99 @@ static inline float govCalcRate(uint16_t param, uint16_t min, uint16_t max)
 }
 
 
+//// Adjustment functions
+
+int adjustmentGet_GOV_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.gain;
+}
+
+void adjustmentSet_GOV_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.gain = value;
+    gov.K  = currentPidProfile->governor.gain / 100.0f;
+}
+
+int adjustmentGet_GOV_P_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.p_gain;
+}
+
+void adjustmentSet_GOV_P_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.p_gain = value;
+    gov.Kp = currentPidProfile->governor.p_gain / 10.0f;
+}
+
+int adjustmentGet_GOV_I_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.i_gain;
+}
+
+void adjustmentSet_GOV_I_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.i_gain = value;
+    gov.Ki = currentPidProfile->governor.i_gain / 10.0f;
+}
+
+int adjustmentGet_GOV_D_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.d_gain;
+}
+
+void adjustmentSet_GOV_D_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.d_gain = value;
+    gov.Kd = currentPidProfile->governor.d_gain / 1000.0f;
+}
+
+int adjustmentGet_GOV_F_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.f_gain;
+}
+
+void adjustmentSet_GOV_F_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.f_gain = value;
+    gov.Kf = currentPidProfile->governor.f_gain / 100.0f;
+}
+
+int adjustmentGet_GOV_TTA_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.tta_gain;
+}
+
+void adjustmentSet_GOV_TTA_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.tta_gain = value;
+    gov.TTAGain = mixerRotationSign() * currentPidProfile->governor.tta_gain / -125.0f;
+    if (gov.mode >= GM_STANDARD)
+        gov.TTAGain /= gov.K * gov.Kp;
+}
+
+int adjustmentGet_GOV_CYCLIC_FF(__unused int adjFunc)
+{
+    return currentPidProfile->governor.cyclic_ff_weight;
+}
+
+void adjustmentSet_GOV_CYCLIC_FF(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.cyclic_ff_weight = value;
+    gov.cyclicWeight = currentPidProfile->governor.cyclic_ff_weight / 100.0f;
+}
+
+int adjustmentGet_GOV_COLLECTIVE_FF(__unused int adjFunc)
+{
+    return currentPidProfile->governor.collective_ff_weight;
+}
+
+void adjustmentSet_GOV_COLLECTIVE_FF(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.collective_ff_weight = value;
+    gov.collectiveWeight = currentPidProfile->governor.collective_ff_weight / 100.0f;
+}
+
+
 //// Interface functions
 
 void governorUpdate(void)
@@ -1035,7 +1128,7 @@ void governorUpdate(void)
     }
 }
 
-void governorInitProfile(const pidProfile_t *pidProfile)
+INIT_CODE void governorInitProfile(const pidProfile_t *pidProfile)
 {
     if (getMotorCount() > 0)
     {
@@ -1075,7 +1168,7 @@ void governorInitProfile(const pidProfile_t *pidProfile)
     }
 }
 
-void governorInit(const pidProfile_t *pidProfile)
+INIT_CODE void governorInit(const pidProfile_t *pidProfile)
 {
     // Must have at least one motor
     if (getMotorCount() > 0)

src/main/pg/adjustments.h
@@ -22,6 +22,9 @@
 #include "pg/pg.h"
 #include "pg/modes.h"
 
+#define ADJFUN_DECLARE(id) \
+    int adjustmentGet_##id(int); \
+    void adjustmentSet_##id(int, int);
 
 typedef struct {
     uint8_t function;

rotorflight/rotorflight-firmware

src/main/flight/governor.c
@@ -34,6 +34,7 @@
 #include "drivers/time.h"
 
 #include "fc/runtime_config.h"
+#include "fc/rc_adjustments.h"
 #include "fc/rc_controls.h"
 #include "fc/rc.h"
 
@@ -227,7 +228,6 @@ static float govMode2Control(void);
 
 static void governorUpdateState(void);
 static void governorUpdatePassthrough(void);
-
 
 
 //// Access functions
@@ -1009,6 +1009,99 @@ static inline float govCalcRate(uint16_t param, uint16_t min, uint16_t max)
 }
 
 
+//// Adjustment functions
+
+int get_ADJUSTMENT_GOV_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.gain;
+}
+
+void set_ADJUSTMENT_GOV_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.gain = value;
+    gov.K  = currentPidProfile->governor.gain / 100.0f;
+}
+
+int get_ADJUSTMENT_GOV_P_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.p_gain;
+}
+
+void set_ADJUSTMENT_GOV_P_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.p_gain = value;
+    gov.Kp = currentPidProfile->governor.p_gain / 10.0f;
+}
+
+int get_ADJUSTMENT_GOV_I_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.i_gain;
+}
+
+void set_ADJUSTMENT_GOV_I_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.i_gain = value;
+    gov.Ki = currentPidProfile->governor.i_gain / 10.0f;
+}
+
+int get_ADJUSTMENT_GOV_D_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.d_gain;
+}
+
+void set_ADJUSTMENT_GOV_D_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.d_gain = value;
+    gov.Kd = currentPidProfile->governor.d_gain / 1000.0f;
+}
+
+int get_ADJUSTMENT_GOV_F_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.f_gain;
+}
+
+void set_ADJUSTMENT_GOV_F_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.f_gain = value;
+    gov.Kf = currentPidProfile->governor.f_gain / 100.0f;
+}
+
+int get_ADJUSTMENT_GOV_TTA_GAIN(__unused int adjFunc)
+{
+    return currentPidProfile->governor.tta_gain;
+}
+
+void set_ADJUSTMENT_GOV_TTA_GAIN(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.tta_gain = value;
+    gov.TTAGain = mixerRotationSign() * currentPidProfile->governor.tta_gain / -125.0f;
+    if (gov.mode >= GM_STANDARD)
+        gov.TTAGain /= gov.K * gov.Kp;
+}
+
+int get_ADJUSTMENT_GOV_CYCLIC_FF(__unused int adjFunc)
+{
+    return currentPidProfile->governor.cyclic_ff_weight;
+}
+
+void set_ADJUSTMENT_GOV_CYCLIC_FF(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.cyclic_ff_weight = value;
+    gov.cyclicWeight = currentPidProfile->governor.cyclic_ff_weight / 100.0f;
+}
+
+int get_ADJUSTMENT_GOV_COLLECTIVE_FF(__unused int adjFunc)
+{
+    return currentPidProfile->governor.collective_ff_weight;
+}
+
+void set_ADJUSTMENT_GOV_COLLECTIVE_FF(__unused int adjFunc, int value)
+{
+    currentPidProfile->governor.collective_ff_weight = value;
+    gov.collectiveWeight = currentPidProfile->governor.collective_ff_weight / 100.0f;
+}
+
+
 //// Interface functions
 
 void governorUpdate(void)
@@ -1035,7 +1128,7 @@ void governorUpdate(void)
     }
 }
 
-void governorInitProfile(const pidProfile_t *pidProfile)
+INIT_CODE void governorInitProfile(const pidProfile_t *pidProfile)
 {
     if (getMotorCount() > 0)
     {
@@ -1075,7 +1168,7 @@ void governorInitProfile(const pidProfile_t *pidProfile)
     }
 }
 
-void governorInit(const pidProfile_t *pidProfile)
+INIT_CODE void governorInit(const pidProfile_t *pidProfile)
 {
     // Must have at least one motor
     if (getMotorCount() > 0)

src/main/pg/adjustments.h
@@ -22,6 +22,9 @@
 #include "pg/pg.h"
 #include "pg/modes.h"
 
+#define ADJFUN_DECLARE(id) \
+    int get_ADJUSTMENT_##id(int); \
+    void set_ADJUSTMENT_##id(int, int);
 
 typedef struct {
     uint8_t function;