25024e57d477859a4b7c012569fd288908ded6a8
Petri Mattila <petri.t.j.mattila@gmail.com>
2025-10-04 12:05:37 +0100

    Governor refactor (#314)
    Please read docs/Governor.md for details

--- src/main/pg/pid.h
+++ src/main/pg/pid.h
@@ -77,7 +77,12 @@ typedef struct {
 } pidTrainerMode_t;
 
 typedef struct {
+    uint32_t    flags;
     uint16_t    headspeed;
+    uint8_t     min_throttle;
+    uint8_t     max_throttle;
+    uint8_t     idle_throttle;
+    uint8_t     auto_throttle;
     uint8_t     gain;
     uint8_t     p_gain;
     uint8_t     i_gain;
@@ -89,11 +94,12 @@ typedef struct {
     uint8_t     f_limit;
     uint8_t     tta_gain;
     uint8_t     tta_limit;
-    uint8_t     yaw_ff_weight;
-    uint8_t     cyclic_ff_weight;
-    uint8_t     collective_ff_weight;
-    uint8_t     max_throttle;
-    uint8_t     min_throttle;
+    uint8_t     yaw_weight;
+    uint8_t     cyclic_weight;
+    uint8_t     collective_weight;
+    uint8_t     collective_curve;
+    uint8_t     fallback_drop;
+    uint8_t     dyn_min_level;      // TDB remove
 } governorProfile_t;
 
 typedef struct {
