a4c8d146f1ef3dd7324443a94f9c09d9bc9782e2
Rob Thomson <rob@marotori.com>
2026-01-12 15:35:20 +0000

    Remove stray comment

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -1400,7 +1400,6 @@ static bool mspProcessOutCommand(int16_t cmdMSP, sbuf_t *dst)
         break;
 
     case MSP_ADJUSTMENT_RANGES:
-        // If you send a command, we skip this and use the call in MSP_ADJUSTMENT_RANGES mspFcProcessOutCommandWithArg()
         for (int i = 0; i < MAX_ADJUSTMENT_RANGE_COUNT; i++) {
             const adjustmentRange_t *adjRange = adjustmentRanges(i);
             sbufWriteU8(dst, adjRange->function);
