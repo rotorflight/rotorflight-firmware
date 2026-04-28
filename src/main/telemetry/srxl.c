34d0d04f6bc88b915abf47c910b63b2654764690
Anders Hoglund <AndWho@gmail.com>
2026-04-28 14:27:45 +0200

    Revert PR403. Caused glitches with Spektrum SRXL.

--- src/main/telemetry/srxl.c
+++ src/main/telemetry/srxl.c
@@ -848,10 +848,6 @@ static void processSrxl(timeUs_t currentTimeUs)
     sbuf_t *dst = &srxlPayloadBuf;
     srxlScheduleFnPtr srxlFnPtr;
 
-    bool tryNextUserFrame = true;
-    uint8_t tryNextUserCount = 0;
-
-  do {
     if (srxlScheduleIndex < SRXL_SCHEDULE_MANDATORY_COUNT) {
         srxlFnPtr = srxlScheduleFuncs[srxlScheduleIndex];
     } else {
@@ -873,14 +869,9 @@ static void processSrxl(timeUs_t currentTimeUs)
         srxlInitializeFrame(dst);
         if (srxlFnPtr(dst, currentTimeUs)) {
             srxlFinalize(dst);
-            tryNextUserFrame = false;
-            srxlScheduleIndex = (srxlScheduleIndex + 1) % SRXL_SCHEDULE_COUNT_MAX;
-        } else {
-            tryNextUserFrame = true;
-            tryNextUserCount++;
         }
     }
-  } while (tryNextUserFrame && tryNextUserCount < SRXL_SCHEDULE_USER_COUNT);
+    srxlScheduleIndex = (srxlScheduleIndex + 1) % SRXL_SCHEDULE_COUNT_MAX;
 }
 
 void initSrxlTelemetry(void)
