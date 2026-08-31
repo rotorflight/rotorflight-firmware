1e7c87c16864dbbd5abcde3e6da86371f1262049
Anders Hoglund <AndWho@gmail.com>
2026-08-31 11:19:55 +0200

    Init glitch on bind pin removed.

--- src/main/rx/spektrum.c
+++ src/main/rx/spektrum.c
@@ -271,26 +271,23 @@ void spektrumBind(rxConfig_t *rxConfig)
         }
     }
 
-    // Bind window is around 20-140ms after powerup
-    // Delay to make sure all IO activity happens within bind wimdow
-    delay(50);
-
     IO_t bindIO = IOGetByTag(bindPin);
 
     IOInit(bindIO, OWNER_RX_BIND, 0);
-    // Init RX line, wil be set low
+
+    // Prepare bind pin, set output register high before setting it as an output to avoid glitch.
+    IOInit(bindIO, OWNER_RX_BIND, 0);
+    IOConfigGPIO(bindIO, IOCFG_IN_FLOATING); // doesn't change configuration, just enables the GPIO clock
+    IOWrite(bindIO, true);
     IOConfigGPIO(bindIO, IOCFG_OUT_PP);
 
     LED1_ON;
 
-    // RX line, set high
-    IOWrite(bindIO, true);
-
-    delay(10);
+    // Bind window is around 20-140ms after powerup
+    delay(60);
     LED1_OFF;
 
-    // Issue requested number of bindpulses adjust by -1 for initial low pulse
-    for (int i = 0; i < rxConfig->spektrum_sat_bind-1; i++) {
+    for (int i = 0; i < rxConfig->spektrum_sat_bind; i++) {
         LED0_OFF;
         LED2_OFF;
         // RX line, drive low for 120us
